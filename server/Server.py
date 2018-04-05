44b734a9ca5e6b8557a8a3187cb2483bc1dcb1e6
VincentsCode <vincent.schmandt@gmx.de>
2018-04-05 12:04:52 +0200

    Added Offline-Actor Support

--- server/Server.py
+++ server/Server.py
@@ -205,7 +205,10 @@ for actor_name in actors:
 
 # CONNECT TO ACTORS
 for actor in actors:
-    actors[actor].connect()
+	try:
+        actors[actor].connect()
+	except Exception:
+		print(actor, "offline")
 
 # START UI-SERVER
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
