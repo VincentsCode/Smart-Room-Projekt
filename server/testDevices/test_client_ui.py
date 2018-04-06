16a194ce74bff54c91794551764521d909d2a2d6
FabianJuforge <33459250+FabianJuforge@users.noreply.github.com>
2018-04-06 15:11:33 +0200

    added app

--- server/testDevices/test_client_ui.py
+++ server/testDevices/test_client_ui.py
@@ -1,12 +1,14 @@
+# IMPORTS
 import socket
 import Constants
 
+# CONNECT TO SERVER
 ip = "localhost"
 port = 2222
-
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((ip, port))
 
+# GET INFO
 msg = Constants.UI_CLIENT_DATA_REQUEST
 while len(bytes(msg, "utf8")) < 32:
     msg += "#"
@@ -15,15 +17,7 @@ answer = socket.recv(2048)
 s_a = str(answer, "utf8")
 print(s_a)
 
-# EXAMPLE: UI_CLIENT_COMMAND = "CMD_Computer0_2##################"
-msg = Constants.UI_CLIENT_COMMAND_IDENTIFIER + "Computer0" + "_" + "1"
-while len(bytes(msg, "utf8")) < 32:
-    msg += "#"
-socket.sendall(bytes(msg, "utf8"))
-answer = socket.recv(2048)
-s_a = str(answer, "utf8")
-print(s_a)
-
+# SEND COMMAND
 msg = Constants.UI_CLIENT_COMMAND_IDENTIFIER + "Computer0" + "_" + "0"
 while len(bytes(msg, "utf8")) < 32:
     msg += "#"
---
+++ ui/androidsmartroom/app/src/main/java/com/example/fabian/androidsmartroom/SettingsActivity.java
@@ -0,0 +1,23 @@
+package com.example.fabian.androidsmartroom;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    public void main_menu(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+}
