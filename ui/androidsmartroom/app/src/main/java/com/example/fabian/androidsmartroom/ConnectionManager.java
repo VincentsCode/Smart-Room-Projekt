fdee813a276c2a8c2779314b1529036cb7e0bc3a
VincentsCode <vincent.schmandt@gmx.de>
2018-04-10 14:24:33 +0200

    Added throw

--- ui/androidsmartroom/app/src/main/java/com/example/fabian/androidsmartroom/ConnectionManager.java
+++ ui/androidsmartroom/app/src/main/java/com/example/fabian/androidsmartroom/ConnectionManager.java
@@ -9,36 +9,32 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class ConnectionManager {
-    public static String send(String message) {
+    public static String send(String message) throws IOException {
         String serverName = "192.168.2.109";
         String str = "";
         int port = 2222;
-        try {
-            Socket client = new Socket(serverName, port);
-            System.out.println("connected to " + client.getRemoteSocketAddress());
+		Socket client = new Socket(serverName, port);
+		System.out.println("connected to " + client.getRemoteSocketAddress());
 
-            OutputStream outToServer = client.getOutputStream();
-            InputStream getFromServer = client.getInputStream();
-            DataOutputStream out = new DataOutputStream(outToServer);
-            DataInputStream input = new DataInputStream(getFromServer);
+		OutputStream outToServer = client.getOutputStream();
+		InputStream getFromServer = client.getInputStream();
+		DataOutputStream out = new DataOutputStream(outToServer);
+		DataInputStream input = new DataInputStream(getFromServer);
 
-            String msg = message;
+		String msg = message;
 
-            while(msg.getBytes(StandardCharsets.UTF_8).length < 64) {
-                msg += "#";
-            }
+		while(msg.getBytes(StandardCharsets.UTF_8).length < 64) {
+			msg += "#";
+		}
 
-            out.write(msg.getBytes(StandardCharsets.UTF_8));
+		out.write(msg.getBytes(StandardCharsets.UTF_8));
 
-            byte[] inputbyte = new byte[2048];
-            input.read(inputbyte, 0, 2048);
-            System.out.println(inputbyte);
-            str = new String(inputbyte, StandardCharsets.UTF_8);
+		byte[] inputbyte = new byte[2048];
+		input.read(inputbyte, 0, 2048);
+		System.out.println(inputbyte);
+		str = new String(inputbyte, StandardCharsets.UTF_8);
 
-            client.close();
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
+		client.close();
         return str;
     }
 
